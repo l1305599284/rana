c429209558a8c3756a489f74e5f19479ccd34b62
trevorXu <xymzhs@gmail.com>
2022-07-25 17:57:53 +0800

    feat : add projections

---
+++ lib/projection.ts
@@ -0,0 +1,15 @@
+import { scale, tl } from "./transform";
+
+export function orthographic(
+  l: number,
+  r: number,
+  b: number,
+  t: number,
+  n: number,
+  f: number,
+  leftHand?: true
+) {
+  const tra = tl(-(l + r) / 2, -(t + b) / 2, -(f + n) / 2);
+  const sca = scale(2 / (r - l), 2 / (t - b), 2 / (f - n));
+  return sca.mul(tra);
+}
