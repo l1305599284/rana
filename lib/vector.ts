ea5410c09562b632c0eb5a0917676fe138328591
trevorXu <xymzhs@gmail.com>
2022-07-26 20:33:48 +0800

    feat : add vector

--- lib/vector.ts
+++ lib/vector.ts
@@ -2,24 +2,85 @@ import { DType } from "./matrix";
 
 export class Vector {
   data: Float32Array;
+  ndim: number = 4;
   dtype: DType = "f32";
   byteLength: number;
-  constructor(x: number, y: number, z: number = 0, w: number = 1) {
-    this.data = new Float32Array([x, y, z, w]);
-
+  constructor(
+    x: number,
+    y: number,
+    z: number = 0,
+    w: number = 1,
+    dtype: DType = "f32"
+  ) {
+    if (dtype == "f32") this.data = new Float32Array([x, y, z, w]);
     this.byteLength = this.data.byteLength;
   }
   set(data: number[]) {
-    this.data = new Float32Array(data);
+    if (this.dtype == "f32") this.data = new Float32Array(data);
+  }
+  mul(v: Vector) {
+    this.data[0] *= v.data[0];
+    this.data[1] *= v.data[1];
+    this.data[2] *= v.data[2];
+    return this;
+  }
+  add(v: Vector) {
+    this.data[0] += v.data[0];
+    this.data[1] += v.data[1];
+    this.data[2] += v.data[2];
+    return this;
+  }
+  sub(v: Vector) {
+    this.data[0] -= v.data[0];
+    this.data[1] -= v.data[1];
+    this.data[2] -= v.data[2];
+    return this;
+  }
+  times(t: number) {
+    this.data[0] *= t;
+    this.data[1] *= t;
+    this.data[2] *= t;
+    return this;
+  }
+  norm() {
+    return Math.sqrt(
+      this.data.reduce((a, v, i) => {
+        a += v ** 2;
+        return a;
+      }, 0)
+    );
+  }
+  normalizing() {
+    const n = this.norm();
+    this.data[0] /= n;
+    this.data[1] /= n;
+    this.data[2] /= n;
+    return this;
+  }
+  dot(v: Vector) {
+    return (
+      this.data[0] * v.data[0] +
+      this.data[1] * v.data[1] +
+      this.data[2] * v.data[2]
+    );
+  }
+  cos(v: Vector) {
+    return (this.dot(v) / this.norm()) * v.norm();
   }
 }
 
-export function vec2(x: number, y: number) {
-  return new Vector(x, y);
+export function vec2(x: number, y: number, dtype: DType = "f32") {
+  return new Vector(x, y, 0, 1, dtype);
 }
-export function vec3(x: number, y: number, z: number) {
-  return new Vector(x, y, z);
+export function vec3(x: number, y: number, z: number, dtype: DType = "f32") {
+  return new Vector(x, y, z, 1, dtype);
 }
-export function vec4(x: number, y: number, z: number, w: number) {
-  return new Vector(x, y, z, w);
+export function vec4(
+  x: number,
+  y: number,
+  z: number,
+  w: number,
+  dtype: DType = "f32"
+) {
+  return new Vector(x, y, z, w, dtype);
 }
