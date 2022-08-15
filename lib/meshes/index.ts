10241640770d780f6b7a55414fbf899aa474f3e6
trevorXu <xymzhs@gmail.com>
2022-08-15 09:28:31 +0800

    feat : add meshes including box & sphere

---
+++ lib/meshes/index.ts
@@ -0,0 +1,8 @@
+import * as boxMeshes from "./box";
+import * as sphereMeshes from "./sphere";
+export function box() {
+  return boxMeshes;
+}
+export function sphere() {
+  return sphereMeshes;
+}
